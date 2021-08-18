00e80e8ef78d3efd048872696a8ca38265f24813
NotStranger <85306664+AhmedHoath@users.noreply.github.com>
2021-08-17 22:03:04 -0600

    Add files via upload

---
+++ Block Dodger/conf.lua
@@ -0,0 +1,8 @@
+function love.conf(t)
+    t.window.title = 'Block Dodger 1'
+    t.window.width = 1000
+    t.window.height = 750
+    t.window.display = 1
+    t.window.fullscreen = false
+    t.window.resizable = false
+end
